8d08dda9b104b36fd279b7d2fd7b1bcaa1cbfe13
Houtao Deng <softwaredeng@gmail.com>
2015-03-22 00:37:38 -0700

    save

--- Test/Test.r
+++ Test/Test.r
@@ -1,71 +1,79 @@
-# Citation
-# Houtao Deng, Interpreting Tree Ensembles with inTrees, Houtao Deng, arXiv:1408.5456, 2014
-# The code below produces the results in the paper above (randomness of the tree ensembles can lead to slight differences of the results)
+# This is file is temporary
+
 
 rm(list=ls(all=TRUE))
-library(inTrees);
 library(randomForest);
+library(RRF);
 graphics.off()
 
-# The regular expression are used to find a match with the ending characters of .R, .r
-## If you want to source() a bunch of files, something like## the following may be useful:
 sourceDir <- function(path, trace = TRUE) {
   for (nm in list.files(path, pattern = "\\.[Rr]$")) {
-    if(trace) cat(nm,":")
+    if(trace) cat(nm)
     source(file.path(path, nm))
     if(trace) cat("\n")
   }
 }
 
-sourceDir("R/")
+sourceDir("devR/")
 
 nRep <- 10 # in the paper it is set to be 100
 
-dataV <- c("iris.data","auto.data","breast.data","pima.data","heart.data","german.data","waveform.data","horse.data","austra.data",
-"crx.data","vehicle.data","labor.data","wine.data","zoo.data","tic-tac.data","hepati.data","lymph.data","anneal.data",
-"glass.data","led7.data")
-
-eAll <- NULL;fAll <- NULL
-
-for(dataI in 1:length(dataV)){
-thisData <- dataV[dataI]
-print(thisData)
+thisData <- c("german.data")
 path <- paste(getwd(), "/data/",thisData,sep=""); #musk vehicle is good austra
-
-# R 3.1.1 seems to have a bug on parsing '?' as NA when it is in the begining of a row
-# hence use a temporary solution below
 X <- read.table(path,header=TRUE,sep = ",")
 X[X[,]=="?"] <- NA
-write.table(X,"tmp.txt",col.names = TRUE,quote=FALSE,sep = ",")
-X <- read.table("tmp.txt",header=TRUE,sep = ",",na.strings <- c(''))
-
+X <- na.roughfix(X)#Impute Missing Values by median/mode
 Y <-  X[,ncol(X)]
 X <- X[,-ncol(X)]
-for(ii in ncol(X):1){ # remove attributes with 1 or 0 level
- if( length(unique(X[!is.na(X[,ii]),ii])) <= 1  ) X <- X[,-ii] 
-}
-
-X <- na.roughfix(X)#Impute Missing Values by median/mode
-
-for(foldI in 1:nRep)
-{
-ixTrain <- sample(1:nrow(X),round(2*nrow(X)/3),replace=FALSE)
-ixTest <- setdiff(1:nrow(X),ixTrain)
-
-testX <- X[ixTest,];trainX <- X[ixTrain,]
-testY <- as.character(Y[ixTest]);trainY <- as.character(Y[ixTrain])
 
 #Orig RF and calculate the importance
-rf <- randomForest(trainX, as.factor(trainY),ntree=100) 
+rf <- randomForest(X, as.factor(Y),ntree=100) 
 
 treeList <- RF2List(rf)
-ruleExec <- extractRules(treeList,trainX) 
+ruleExec <- extractRules(treeList,X) 
 ruleExec <- unique(ruleExec) # remove same rules. NOTE: for variable interaction analysis, you should NOT perform this step
-
 ix <- sample(1:length(ruleExec),min(2000,length(ruleExec))) #randomly select 2000 rules
 ruleExec <- ruleExec[ix,,drop=FALSE]
+ruleMetric <- getRuleMetric(ruleExec,X,Y)
+ruleMetric <- selectRuleRRF(ruleMetric,X,Y)
 
-ruleMetric <- getRuleMetric(ruleExec,trainX,trainY)
+# cvob1=cv.glmnet(as.matrix(X[,,drop=FALSE]),Y, type.measure="mae")
+library(glmnet)
+Y <- as.numeric(Y)
+if (is.numeric(Y) == FALSE) stop("Target variable needs to be numerical!")
+# if (length(unique(Y)) > 2) stop("Currently only support two class problem!")
+
+
+ruleI = sapply(ruleMetric[,"condition"],rule2Table,X,Y)
+colnamesSave <- colnames(ruleI)
+colnames(ruleI) <- paste0("R_",1:ncol(ruleI))
+glmModel <- cv.glmnet(as.matrix(ruleI),Y, type.measure="mae")
+coef <- coef(glmModel)
+
+grepl("^[^_]+_2",s)
+
+ix.rules <- which( grepl("R_",rownames(coef) ))
+ix.non.zero <- which (as.numeric(coef) != 0)
+ix.effective.rules <- intersect(ix.rules, ix.non.zero)
+coef[ix.effective.rules,]
+
+
+coefReg <- 0.95 - 0.01*as.numeric(ruleMetric[,"len"])/max(as.numeric(ruleMetric[,"len"]))
+rf <- RRF(ruleI,as.factor(target), flagReg = 1, coefReg=coefReg, mtry = (ncol(ruleI)*1/2) , ntree=50, maxnodes= 10,replace=FALSE) 
+imp <- rf$importance/max(rf$importance)
+feaSet <- which(imp > 0.01)
+
+ruleSetPrunedRRF <- cbind(ruleMetric[feaSet,,drop=FALSE],impRRF=imp[feaSet])
+ix = order(as.numeric(ruleSetPrunedRRF[,"impRRF"]),
+           - as.numeric(ruleSetPrunedRRF[,"err"]),
+           - as.numeric(ruleSetPrunedRRF[,"len"]),
+           decreasing=TRUE)
+ruleSelect <- ruleSetPrunedRRF[ix,,drop=FALSE]
+
+glmModel <- cv.glmnet(as.matrix(X),Y, type.measure="mae")
+coef <- coef(glmModel)
+pred <- predict(glmModel,as.matrix(X[,])) #ixTest
+
 ruleMetric <- pruneRule(ruleMetric,trainX,trainY,typeDecay = 1)
 ruleMetric <- unique(ruleMetric)
 
@@ -73,30 +81,4 @@ ruleClassifier <- buildLearner(ruleMetric,trainX,trainY)
 readable <- presentRules(ruleClassifier,colnames(trainX))
 pred <- applyLearner(ruleClassifier,testX)
 errRFInTrees=1-sum(pred==testY)/length(pred);
-
-library(rpart)
-trainX <- data.frame(trainX)
-D <- data.frame(cbind(trainX,trainY))
-D[,"trainY"] <- as.factor(D[,"trainY"])
-rp <- rpart(trainY ~. , D)
-pred <- as.character(predict(rp,data.frame(testX),type="class"))
-errRPART <- 1-sum(pred==testY)/length(pred);
-
-thisE <- c(errRFInTrees, errRPART)
-cat("foldI",foldI,thisE,"-  ")
-eAll <- rbind(eAll,thisE)
-
-}
-}
-colnames(eAll) <- c("STEL","rpart")
-
-# avearge error for each data set
-aveMat <- NULL
-nRep <- nrow(eAll)/length(dataV)
-for(I in 1:length(dataV))
-{
-thisE <- eAll[((I-1)*nRep+1):(I*nRep),]
-aveMat <- rbind(aveMat,apply(thisE,2,mean))
-}
-aveMat - aveMat[,1]
 
