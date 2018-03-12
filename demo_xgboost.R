5c7b6ddad085ef7540328895cfa58dc6181d6167
softwaredeng <houtao@instacart.com>
2018-03-11 22:07:55 -0700

    handle empty xgboost tree

--- demo_xgboost.R
+++ demo_xgboost.R
@@ -11,8 +11,8 @@ rule_exec <- extractRules(tree_list,model_mat,digits=3)
 rule_exec <- unique(rule_exec) # remove same rules. NOTE: for variable interaction analysis, you should NOT perform this step
 ix <- sample(1:length(rule_exec),min(2000,length(rule_exec))) #randomly select 2000 rules
 rule_exec <- rule_exec[ix,,drop=FALSE]
-rule_metric <- getrule_metric(rule_exec,model_mat,Y)
-freq_patterns <- getFreqPattern(rule_metric)
+rule_metric <- getRuleMetric(rule_exec,model_mat,Y)
+# freq_patterns <- getFreqPattern(rule_metric,maxlen = 3) # can be less useful for numeric variables.
 rule_metric <- pruneRule(rule_metric,model_mat,Y,typeDecay = 1)
 rule_select <- selectRuleRRF(rule_metric, model_mat, Y)
 rule_metric <- unique(rule_metric)
