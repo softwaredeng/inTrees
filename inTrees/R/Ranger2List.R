4494239712cfafc719d87ba52c68871ad4df7949
softwaredeng <houtao@instacart.com>
2018-03-12 21:20:56 -0700

    add ranger

---
+++ inTrees/R/Ranger2List.R
@@ -0,0 +1,23 @@
+require(ranger)
+Ranger2List <- function(rf_ranger)
+{
+  formatRanger <- function(tree){
+    rownames(tree) <- 1:nrow(tree)
+    tree$`status` <- ifelse(tree$`terminal`==TRUE,-1,1)
+    tree$`left daughter` <- tree$`leftChild` + 1
+    tree$`right daughter` <- tree$`rightChild` + 1
+    tree$`split var` <- tree$`splitvarID`
+    tree$`split point` <- tree$`splitval`
+    tree$`prediction` <- tree$`prediction`
+    tree <- tree[,c("left daughter","right daughter","split var","split point","status")]
+    tree <- as.data.frame(tree)
+    return(tree)
+  }
+  treeList <- NULL
+  treeList$ntree <- rf_ranger$num.trees
+  treeList$list <- vector("list",rf_ranger$num.trees)
+  for(i in 1:rf_ranger$num.trees){
+    treeList$list[[i]] <- formatRanger( treeInfo(rf_ranger, tree = i) )
+  }
+  return(treeList)
+}
