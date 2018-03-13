275d992af2fd22883c36f2229487648503d810a2
softwaredeng <houtao@instacart.com>
2018-03-12 22:47:38 -0700

    add ranger support

--- demo_ranger.R
+++ demo_ranger.R
@@ -1,20 +1,31 @@
 rm(list= ls())
-library(randomForest)
+library(ranger)
 library(inTrees)
+library(RRF)
 
-# data set 1
+sourceDir <- function(path, trace = TRUE) {
+  for (nm in list.files(path, pattern = "\\.[Rr]$")) {
+    if(trace) cat(nm)
+    source(file.path(path, nm))
+    if(trace) cat("\n")
+  }
+}
+
+sourceDir("Test/devR/")
+
+# data 1
 X <- within(iris,rm("Species")); Y <- iris[,"Species"]
-rf <- randomForest(X, as.factor(Y),ntree=100) 
+rf_ranger <- ranger(Species ~ ., data = iris)
 
 # data set 2
 path <- paste(getwd(), "/Test/data/german.data",sep="") #musk vehicle is good austra
 data <- read.table(path,header=TRUE,sep = ",")
 X <- within(data,rm("Y")); Y <- data$Y
-rf <- randomForest(X, as.factor(Y),ntree=100) 
+rf_ranger <- ranger(Y ~ ., data = data)
 
-tree_list <- RF2List(rf)
+tree_list <- Ranger2List(rf_ranger)
 
-rule_exec <- extractRules(tree_list,X,digits=3) 
+rule_exec <- extractRules(tree_list,X,digits=10) 
 rule_exec <- unique(rule_exec) # remove same rules. NOTE: for variable interaction analysis, you should NOT perform this step
 ix <- sample(1:length(rule_exec),min(2000,length(rule_exec))) #randomly select 2000 rules
 rule_exec <- rule_exec[ix,,drop=FALSE]
@@ -27,4 +38,5 @@ rule_metric <- unique(rule_metric)
 rule_classifier <- buildLearner(rule_metric,X,Y)
 readable <- presentRules(rule_classifier,colnames(X),digits=3)
 pred <- applyLearner(rule_classifier,X)
+print(readable)
 print( 1-sum(pred==Y)/length(pred) )
